ab1b564fbb9dda5bd00164ec06144ade37fff5d6
Lee Jian <209rrsz@gmail.com>
2019-05-19 21:37:44 +0900

    heroku 배포를 위한 수정

--- summercoding/settings.py
+++ summercoding/settings.py
@@ -122,6 +122,11 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'todo/static'),
